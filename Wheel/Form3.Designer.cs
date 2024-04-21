c05652a6a71306ad954cb3a4c155b6a212b0f606
user89986 <167529891+user89986@users.noreply.github.com>
2024-04-21 18:26:25 +0300

    edit3

--- Wheel/Form3.Designer.cs
+++ Wheel/Form3.Designer.cs
@@ -51,7 +51,6 @@
             this.User.Name = "User";
             this.User.Size = new System.Drawing.Size(300, 20);
             this.User.TabIndex = 1;
-            this.User.TextChanged += new System.EventHandler(this.User_TextChanged);
             // 
             // label1
             // 
