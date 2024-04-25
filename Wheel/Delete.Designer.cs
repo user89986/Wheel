2defb3489d8b7350fe09e73b032033791943ef5f
user89986 <167529891+user89986@users.noreply.github.com>
2024-04-25 14:11:20 +0300

    fix

--- Wheel/Delete.Designer.cs
+++ Wheel/Delete.Designer.cs
@@ -62,9 +62,9 @@
             this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(4, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 18);
+            this.label1.Size = new System.Drawing.Size(139, 18);
             this.label1.TabIndex = 16;
-            this.label1.Text = "Имя*";
+            this.label1.Text = "Имя пользователя*";
             // 
             // Surname1
             // 
@@ -86,9 +86,9 @@
             this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.Location = new System.Drawing.Point(4, 72);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 18);
+            this.label2.Size = new System.Drawing.Size(171, 18);
             this.label2.TabIndex = 19;
-            this.label2.Text = "Фамилия*";
+            this.label2.Text = "Фамилия пользователя*";
             // 
             // label3
             // 
@@ -96,9 +96,9 @@
             this.label3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.Location = new System.Drawing.Point(4, 116);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(84, 18);
+            this.label3.Size = new System.Drawing.Size(181, 18);
             this.label3.TabIndex = 20;
-            this.label3.Text = "Отчество*";
+            this.label3.Text = "Отчество пользователя*";
             // 
             // Delete
             // 
--- Wheel/Delete.cs
+++ Wheel/Delete.cs
@@ -66,6 +66,6 @@ namespace Wheel
             }
         }
 
-
+  
     }
 }
